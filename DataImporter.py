11ba89d901e8b19cf4d0d6829c489a8567468048
aidanoneill3776 <aidanoneill3776@gmail.com>
2019-04-02 14:19:18 -0400

    Updates based on hammurabi's email

--- DataImporter.py
+++ DataImporter.py
@@ -9,71 +9,60 @@ LAST_YEAR = 2018
 # Add match dictionary
 # Foreign keys? John Dale wants to know
 def main():
-    tourney_dict = {}
-    player_dict = {}
-    match_dict = {}
-    for i in range(FIRST_YEAR, LAST_YEAR + 1):
-        data = 'tennis_atp-master/atp_matches_' + str(i) + '.csv'
-        parsed_data = read_data(data, tourney_dict, player_dict, match_dict)
     client = MongoClient()
     db = client['tennis']
-    tourney = db['tourney']
-    insert_id = 1
-    for id in tourney_dict:
-        tourney_instance = tourney_dict[id]
-        tourney_insert = {
-            "ID": id,
-            "Name": tourney_instance[0],
-            "Surface": tourney_instance[1],
-            "Date": tourney_instance[2]
-        }
-        tourney.insert_one(tourney_insert)
-        insert_id += 1
-    # for id in tourney_dict:
-    #     print tourney_dict[id]
-    # for id in player_dict:
-    #     print player_dict[id]
-    # for id in match_dict:
-    #     print match_dict[id]
+    tourneys = db['tourneys']
+    matches = db['matches']
+    players = db['players']
+    for i in range(FIRST_YEAR, LAST_YEAR + 1):
+        data = 'tennis_atp-master/atp_matches_' + str(i) + '.csv'
+        parsed_data = read_data(data, tourneys, matches, players)
 
 
-def read_data(filename, tourney_dict, player_dict, match_dict):
+def read_data(filename, tourneys, matches, players):
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file, delimiter=',')
         for line in csv_reader:
             tourney_id = line[0]
-            # print line
+            tourney_name = line[1]
+            surface = line[2]
+            tourney_date = line[5]
+            result = db.profiles.create_index(
+                [('user_id', pymongo.ASCENDING)], unique=True)
             if tourney_id not in tourney_dict:
-                tourney_name = line[1]
-                surface = line[2]
-                tourney_date = line[5]
+                num_tourneys += 1
                 tourney_dict.update({
-                    tourney_id: (tourney_name, surface, tourney_date)
+                    tourney_id: (tourney_name, surface, tourney_date,
+                                 num_tourneys)
                 })
             winner_id = line[7]
             if winner_id not in player_dict:
+                num_players += 1
                 name = line[10]
                 dominant_hand = line[11]
                 height = line[12]
                 country = line[13]
                 rank = line[15]
                 player_dict.update({
-                    winner_id: (name, dominant_hand, height, country, rank)
+                    winner_id: (name, dominant_hand, height, country, rank,
+                                num_players)
                 })
             loser_id = line[17]
             if loser_id not in player_dict:
+                num_players += 1
                 name = line[20]
                 dominant_hand = line[21]
                 height = line[22]
                 country = line[23]
                 rank = line[24]
                 player_dict.update({
-                    loser_id: (name, dominant_hand, height, country, rank)
+                    loser_id: (name, dominant_hand, height, country, rank,
+                               num_players)
                 })
             match_id = line[6]
             if match_id not in match_dict:
-                winner = winner_id
-                loser = loser_id
+                winner = players[winner_id][5]
+                loser = players[loser_id][5]
                 winner_seed = line[8]
                 loser_seed = line[18]
                 match_dict.update({
