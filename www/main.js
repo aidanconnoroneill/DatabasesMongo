7393782e300ca86b0975f91f3fd1522b0c119f69
Julian Bertini <julianbertini@gmail.com>
2019-04-04 21:58:06 -0400

    lot of the sorting functionality

--- www/main.js
+++ www/main.js
@@ -1,9 +1,9 @@
 var customValues = {
-    playerName: "",
-    playerHand: "",
-    playerHeight: "",
-    playerCountry: "",
-    playerRank: ""
+    playerName: ["", ".name-td"],
+    playerHand: ["", ".hand-td"],
+    playerHeight: ["", ".height-td"],
+    playerCountry: ["", ".country-td"],
+    playerRank: ["",".rank-td"]
 };
 
 $(document).ready ( function() {
@@ -11,43 +11,70 @@ $(document).ready ( function() {
     updateCustomValues();
 
     $( ".search_button" ).click(function() {
-
         getCustomData();
+      });
+    $( ".get_all_button" ).click(function() {
+        getAllData();
       });
 
 })
 
+filterCurrentCol = function () {
+    var nameTuples = $(customValues.playerName[1]);
+    var handTuples = $(customValues.playerHand[1]);
+    var heightTuples = $(customValues.playerHeight[1]);
+    var countryTuples = $(customValues.playerCountry[1]);
+    var rankTuples = $(customValues.playerRank[1]);
+
+    for (i = 0; i < nameTuples.length; i++) { // all the same size
+        if (nameTuples[i].innerText.toUpperCase().indexOf(customValues.playerName[0]) > -1
+            && handTuples[i].innerText.toUpperCase().indexOf(customValues.playerHand[0]) > -1
+            && heightTuples[i].innerText.toUpperCase().indexOf(customValues.playerHeight[0]) > -1
+            && countryTuples[i].innerText.toUpperCase().indexOf(customValues.playerCountry[0]) > -1
+            && rankTuples[i].innerText.toUpperCase().indexOf(customValues.playerRank[0]) > -1)
+            nameTuples[i].parentNode.style.display = "";
+        else {
+            nameTuples[i].parentNode.style.display = "none";
+        }
+    }
+}
+
 updateCustomValues = function () {
-    $(".player-name").change(function () {
-        customValues.playerName = $(this).val();
-        console.log(customValues.playerName);
+    $(".player-name").keyup(function () {
+        customValues.playerName[0] = $(this).val().toUpperCase();
+        console.log(customValues.playerName[0]);
+        filterCurrentCol();
     })
-    $(".player-hand").change(function () {
-        customValues.playerHand = $(this).val();
-        console.log(customValues.playerHand);
+    $(".player-hand").keyup(function () {
+        customValues.playerHand[0] = $(this).val().toUpperCase();
+        console.log(customValues.playerHand[0]);
+        filterCurrentCol();
     })
-    $(".player-height").change(function () {
-        customValues.playerHeight = $(this).val();
-        console.log(customValues.playerHeight);
+    $(".player-height").keyup(function () {
+        customValues.playerHeight[0] = $(this).val().toUpperCase();
+        console.log(customValues.playerHeight[0]);
+        filterCurrentCol();
     })
-    $(".player-country").change(function () {
-        customValues.playerCountry = $(this).val();
-        console.log(customValues.playerCountry);
+    $(".player-country").keyup(function () {
+        customValues.playerCountry[0] = $(this).val().toUpperCase();
+        console.log(customValues.playerCountry[0]);
+        filterCurrentCol(customValues.playerCountry[0], "country-td");
     })
-    $(".player-rank").change(function () {
-        customValues.playerRank = $(this).val();
-        console.log(customValues.playerRank);
+    $(".player-rank").keyup(function () {
+        customValues.playerRank[0] = $(this).val().toUpperCase();
+        console.log(customValues.playerRank[0]);
+        filterCurrentCol(customValues.playerRank[0], "rank-td");
     })
 }
 
 getCustomData = function () {
-    console.log(customValues.playerRank);
+    console.log(customValues.playerName[0]);
     var search_url = "player_table.php/?custom_search=true" +
-                        "&playerName=" + customValues.playerName +
-                        "&playerHand=" + customValues.playerHand +
-                        "&playerHeight=" + customValues.playerHeight +
-                        "&playerCountry=" + customValues.playerCountry + 
-                        "&playerRank=" + customValues.playerRank
+                        "&playerName=" + customValues.playerName[0] +
+                        "&playerHand=" + customValues.playerHand[0] +
+                        "&playerHeight=" + customValues.playerHeight[0] +
+                        "&playerCountry=" + customValues.playerCountry[0] + 
+                        "&playerRank=" + customValues.playerRank[0]
 
     $.ajax({
         url: search_url,
@@ -55,6 +82,18 @@ getCustomData = function () {
       }).done(function(data) {
         console.log(data);
 
+        $("#data-table").html(data); // remember to call event hanlders again after reloading html
+        updateCustomValues();
+      });
+}
+
+getAllData = function () {
+    console.log("Getting all data");
+    var search_url = "player_table.php"
+    $.ajax({
+        url: search_url,
+        context: document.body
+      }).done(function(data) {
         $("#data-table").html(data); // remember to call event hanlders again after reloading html
         updateCustomValues();
       });
