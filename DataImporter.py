35dd33a1befc3433a566c76ba722d026688e671b
Julian Bertini <julianbertini@gmail.com>
2019-04-05 16:04:31 -0400

    database

--- DataImporter.py
+++ DataImporter.py
@@ -15,17 +15,22 @@ def main():
     tourneys = db['tourneys']
     matches = db['matches']
     players = db['players']
-    result = db.profiles.create_index(
+    result = db.tourneys.create_index(
         [('tourney_id', pymongo.ASCENDING)], unique=True)
+    result = db.players.create_index(
+        [('player_id', pymongo.ASCENDING)], unique=True)
     for i in range(FIRST_YEAR, LAST_YEAR + 1):
         data = 'tennis_atp-master/atp_matches_' + str(i) + '.csv'
-        parsed_data = read_data(data, db, tourneys, matches, players)
+        read_data(data, db, tourneys, matches, players)
 
 
 def read_data(filename, db, tourneys, matches, players):
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file, delimiter=',')
         count = 0
+        tourney_id = -1
+        winner_id = -1
+        loser_id = -1
         for line in csv_reader:
             if count == 0:
                 count += 1
@@ -34,42 +39,62 @@ def read_data(filename, db, tourneys, matches, players):
             tourney_name = line[1]
             surface = line[2]
             tourney_date = line[5]
-            tourney_id = tourneys.insert_one({
-                "name": tourney_name,
-                "surface": surface,
-                "date": tourney_date
-            }).inserted_id
+            try:
+                tourney_id = tourneys.insert_one({
+                    "name": tourney_name,
+                    "surface": surface,
+                    "date": tourney_date
+                }).inserted_id
+            except:
+                tourney_id = tourneys.find_one({
+                    "name": tourney_name,
+                    "surface": surface,
+                    "date": tourney_date
+                })._id
             name = line[10]
             dominant_hand = line[11]
             height = line[12]
             country = line[13]
             rank = line[15]
-            winner_id = players.insert_one({
-                "name": name,
-                "hand": dominant_hand,
-                "height": height,
-                "country": country,
-                "rank": rank
-            }).inserted_id
+            try:
+                winner_id = players.insert_one({
+                    "name": name,
+                    "hand": dominant_hand,
+                    "height": height,
+                    "country": country,
+                    "rank": rank
+                }).inserted_id
+            except:
+                print("duplicate winner")
+                pass
             name = line[20]
             dominant_hand = line[21]
             height = line[22]
             country = line[23]
             rank = line[25]
-            loser_id = players.insert_one({
-                "name": name,
-                "hand": dominant_hand,
-                "height": height,
-                "country": country,
-                "rank": rank
-            }).inserted_id
-            matches.insert_one({
-                "winner": winner_id,
-                "loser": loser_id,
-                "tourney": tourney_id,
-                "winner seed": line[8],
-                "loser seed": line[18]
-            })
+            try:
+                loser_id = players.insert_one({
+                    "name": name,
+                    "hand": dominant_hand,
+                    "height": height,
+                    "country": country,
+                    "rank": rank
+                }).inserted_id
+            except:
+                print("duplicate looser")
+                pass
+            try:
+                matches.insert_one({
+                    "winner": winner_id,
+                    "loser": loser_id,
+                    "tourney": tourney_id,
+                    "winner seed": line[8],
+                    "loser seed": line[18]
+                })
+            except:
+
+                print("duplicate match")
+                pass
 
 
 if __name__ == "__main__":
